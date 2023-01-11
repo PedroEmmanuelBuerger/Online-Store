901a9a1db1ec0176c66b0b84da11afb953e45969
Alexandre <alexandre-linux@Alexandre>
2023-01-11 18:57:42 -0300

    task 4 done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,31 @@
 import React, { Component } from 'react';
+import { getCategories } from '../services/api';
+import Navegation from './Navegation';
 
 export default class Home extends Component {
+  state = {
+    categories: [],
+  };
+
+  async componentDidMount() {
+    const categories = await getCategories();
+    this.setState({
+      categories,
+    });
+  }
+
   render() {
+    const { categories } = this.state;
     return (
       <div>
+        <nav className="lateral">
+          { categories
+            .map((category) => (<Navegation
+              key={ category.id }
+              name={ category.name }
+            />)) }
+        </nav>
+
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
