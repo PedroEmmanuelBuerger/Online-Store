f0639f53a1635199e841fab1f3ac82cf2852868b
Allan Ikeda <allanikeda@outlook.com>
2023-01-11 15:59:40 -0300

    Feature - requisito 02 - pagina inicial

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div>
+        <p data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </p>
+      </div>
+    );
+  }
+}
